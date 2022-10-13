b081db8fb795020b2ab990e014c5c241229c67f8
Carlos Aguilera <18aguilerac@gmail.com>
2022-10-13 01:44:25 -0700

    hopefully didnt fu*k anything up

--- routes/api/search.js
+++ routes/api/search.js
@@ -10,10 +10,6 @@ router.get('/:item', async (req, res) => {
       name: { $regex: item, $options: 'i' },
     });
 
-    if (payload.length <= 0) {
-      throw new Error('No results found.');
-    }
-
     res.status(200).json(payload);
   } catch (err) {
     res.status(400).json({ err });
